ad18369a2e79d16c232cabbbdaa0bb41bf949973
Roberto Pacheco <robertopvc@gmail.com>
2021-11-21 15:31:13 -0300

    a

--- end_node_latency_experiment.py
+++ end_node_latency_experiment.py
@@ -79,7 +79,7 @@ def inferenceTimeExperiment(imgs_files_list, p_tar_list, nr_branch_edge_list):
 
 			# For a given number of branches processed in edge, this loop changes the threshold p_tar configuration.
 			for p_tar in p_tar_list:
-				sendImage(img_path, config.url_edge_no_calib, p_tar, int(5))
+				sendImage(img_path, config.url_edge_no_calib, p_tar, int(2))
 				sys.exit()
 				sendImage(img_path, config.url_edge_overall_calib, p_tar, int(nr_branch_edge))
 				sendImage(img_path, config.url_edge_branches_calib, p_tar, int(nr_branch_edge))
