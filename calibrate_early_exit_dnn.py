413847c9c44ce08ac231c07daeee8700f22f40ae
Roberto Pacheco <robertopvc@gmail.com>
2021-11-23 23:25:56 -0300

    a

--- calibrate_early_exit_dnn.py
+++ calibrate_early_exit_dnn.py
@@ -1714,7 +1714,7 @@ def extract_confidence_data(model, test_loader, val_loader, dataset, p_tar_list,
 input_dim = 224
 batch_size_train = 64
 batch_size_test = 1
-model_id = 7
+model_id = 1
 split_ratio = 0.2
 n_classes = 258
 pretrained = False
