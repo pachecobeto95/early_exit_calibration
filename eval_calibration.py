e6f37b3a76ff92b500d1311fceffcdb483fc4ead
Roberto Pacheco <robertopvc@gmail.com>
2021-11-25 17:41:58 -0300

    adding eval

--- eval_calibration.py
+++ eval_calibration.py
@@ -1210,7 +1210,7 @@ def extract_confidence_data(model, test_loader, val_loader, tempDict, dataset, p
 input_dim = 224
 batch_size_train = 64
 batch_size_test = 1
-model_id = 2
+model_id = 3
 split_ratio = 0.2
 n_classes = 258
 pretrained = False
