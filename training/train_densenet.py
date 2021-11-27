255e16426254c24247cce893e5875968c71eb832
Roberto Pacheco <robertopvc@gmail.com>
2021-11-27 15:43:37 -0300

    a

---
+++ training/train_densenet.py
@@ -0,0 +1,395 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as cp
+from collections import OrderedDict
+import os
+import time
+import torch
+import torchvision as tv
+from torch import nn, optim
+from torch.utils.data.sampler import SubsetRandomSampler
+from models import DenseNet
+
+def _bn_function_factory(norm, relu, conv):
+    def bn_function(*inputs):
+        concated_features = torch.cat(inputs, 1)
+        bottleneck_output = conv(relu(norm(concated_features)))
+        return bottleneck_output
+
+    return bn_function
+
+
+class _DenseLayer(nn.Module):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, efficient=False):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
+                        growth_rate, kernel_size=1, stride=1, bias=False)),
+        self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                        kernel_size=3, stride=1, padding=1, bias=False)),
+        self.drop_rate = drop_rate
+        self.efficient = efficient
+
+    def forward(self, *prev_features):
+        bn_function = _bn_function_factory(self.norm1, self.relu1, self.conv1)
+        if self.efficient and any(prev_feature.requires_grad for prev_feature in prev_features):
+            bottleneck_output = cp.checkpoint(bn_function, *prev_features)
+        else:
+            bottleneck_output = bn_function(*prev_features)
+        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+        return new_features
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
+        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+
+
+class _DenseBlock(nn.Module):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, efficient=False):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features + i * growth_rate,
+                growth_rate=growth_rate,
+                bn_size=bn_size,
+                drop_rate=drop_rate,
+                efficient=efficient,
+            )
+            self.add_module('denselayer%d' % (i + 1), layer)
+
+    def forward(self, init_features):
+        features = [init_features]
+        for name, layer in self.named_children():
+            new_features = layer(*features)
+            features.append(new_features)
+        return torch.cat(features, 1)
+
+
+class DenseNet(nn.Module):
+    r"""Densenet-BC model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 3 or 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+            (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+        small_inputs (bool) - set to True if images are 32x32. Otherwise assumes images are larger.
+        efficient (bool) - set to True to use checkpointing. Much more memory efficient, but slower.
+    """
+    def __init__(self, growth_rate=12, block_config=(16, 16, 16), compression=0.5,
+                 num_init_features=24, bn_size=4, drop_rate=0,
+                 num_classes=10, small_inputs=True, efficient=False):
+
+        super(DenseNet, self).__init__()
+        assert 0 < compression <= 1, 'compression of densenet should be between 0 and 1'
+        self.avgpool_size = 8 if small_inputs else 7
+
+        # First convolution
+        if small_inputs:
+            self.features = nn.Sequential(OrderedDict([
+                ('conv0', nn.Conv2d(3, num_init_features, kernel_size=3, stride=1, padding=1, bias=False)),
+            ]))
+        else:
+            self.features = nn.Sequential(OrderedDict([
+                ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ]))
+            self.features.add_module('norm0', nn.BatchNorm2d(num_init_features))
+            self.features.add_module('relu0', nn.ReLU(inplace=True))
+            self.features.add_module('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1,
+                                                           ceil_mode=False))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+                efficient=efficient,
+            )
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=int(num_features * compression))
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = int(num_features * compression)
+
+        # Final batch norm
+        self.features.add_module('norm_final', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+
+        # Initialization
+        for name, param in self.named_parameters():
+            if 'conv' in name and 'weight' in name:
+                n = param.size(0) * param.size(2) * param.size(3)
+                param.data.normal_().mul_(math.sqrt(2. / n))
+            elif 'norm' in name and 'weight' in name:
+                param.data.fill_(1)
+            elif 'norm' in name and 'bias' in name:
+                param.data.fill_(0)
+            elif 'classifier' in name and 'bias' in name:
+                param.data.fill_(0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.avg_pool2d(out, kernel_size=self.avgpool_size).view(features.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+
+class Meter():
+    """
+    A little helper class which keeps track of statistics during an epoch.
+    """
+    def __init__(self, name, cum=False):
+        """
+        name (str or iterable): name of values for the meter
+            If an iterable of size n, updates require a n-Tensor
+        cum (bool): is this meter for a cumulative value (e.g. time)
+            or for an averaged value (e.g. loss)? - default False
+        """
+        self.cum = cum
+        if type(name) == str:
+            name = (name,)
+        self.name = name
+
+        self._total = torch.zeros(len(self.name))
+        self._last_value = torch.zeros(len(self.name))
+        self._count = 0.0
+
+    def update(self, data, n=1):
+        """
+        Update the meter
+        data (Tensor, or float): update value for the meter
+            Size of data should match size of ``name'' in the initialized args
+        """
+        self._count = self._count + n
+        if torch.is_tensor(data):
+            self._last_value.copy_(data)
+        else:
+            self._last_value.fill_(data)
+        self._total.add_(self._last_value)
+
+    def value(self):
+        """
+        Returns the value of the meter
+        """
+        if self.cum:
+            return self._total
+        else:
+            return self._total / self._count
+
+    def __repr__(self):
+        return '\t'.join(['%s: %.5f (%.3f)' % (n, lv, v)
+            for n, lv, v in zip(self.name, self._last_value, self.value())])
+
+
+def run_epoch(loader, model, criterion, optimizer, epoch=0, n_epochs=0, train=True):
+    time_meter = Meter(name='Time', cum=True)
+    loss_meter = Meter(name='Loss', cum=False)
+    error_meter = Meter(name='Error', cum=False)
+
+    if train:
+        model.train()
+        print('Training')
+    else:
+        model.eval()
+        print('Evaluating')
+
+    end = time.time()
+    for i, (input, target) in enumerate(loader):
+        if train:
+            model.zero_grad()
+            optimizer.zero_grad()
+
+            # Forward pass
+            input = input.cuda()
+            target = target.cuda()
+            output = model(input)
+            loss = criterion(output, target)
+
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+            optimizer.n_iters = optimizer.n_iters + 1 if hasattr(optimizer, 'n_iters') else 1
+
+        else:
+            with torch.no_grad():
+                # Forward pass
+                input = input.cuda()
+                target = target.cuda()
+                output = model(input)
+                loss = criterion(output, target)
+
+        # Accounting
+        _, predictions = torch.topk(output, 1)
+        error = 1 - torch.eq(predictions, target).float().mean()
+        batch_time = time.time() - end
+        end = time.time()
+
+        # Log errors
+        time_meter.update(batch_time)
+        loss_meter.update(loss)
+        error_meter.update(error)
+        print('  '.join([
+            '%s: (Epoch %d of %d) [%04d/%04d]' % ('Train' if train else 'Eval',
+                epoch, n_epochs, i + 1, len(loader)),
+            str(time_meter),
+            str(loss_meter),
+            str(error_meter),
+        ]))
+
+    return time_meter.value(), loss_meter.value(), error_meter.value()
+
+
+def train(data, save, valid_size=5000, seed=None,
+          depth=40, growth_rate=12, n_epochs=300, batch_size=64,
+          lr=0.1, wd=0.0001, momentum=0.9):
+    """
+    A function to train a DenseNet-BC on CIFAR-100.
+    Args:
+        data (str) - path to directory where data should be loaded from/downloaded
+            (default $DATA_DIR)
+        save (str) - path to save the model to (default /tmp)
+        valid_size (int) - size of validation set
+        seed (int) - manually set the random seed (default None)
+        depth (int) - depth of the network (number of convolution layers) (default 40)
+        growth_rate (int) - number of features added per DenseNet layer (default 12)
+        n_epochs (int) - number of epochs for training (default 300)
+        batch_size (int) - size of minibatch (default 256)
+        lr (float) - initial learning rate
+        wd (float) - weight decay
+        momentum (float) - momentum
+    """
+
+    if seed is not None:
+        torch.manual_seed(seed)
+
+    # Make save directory
+    if not os.path.exists(save):
+        os.makedirs(save)
+    if not os.path.isdir(save):
+        raise Exception('%s is not a dir' % save)
+
+    # Get densenet configuration
+    if (depth - 4) % 3:
+        raise Exception('Invalid depth')
+    block_config = [(depth - 4) // 6 for _ in range(3)]
+
+    # Data transforms
+    mean = [0.5071, 0.4867, 0.4408]
+    stdv = [0.2675, 0.2565, 0.2761]
+    train_transforms = tv.transforms.Compose([
+        tv.transforms.RandomCrop(32, padding=4),
+        tv.transforms.RandomHorizontalFlip(),
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize(mean=mean, std=stdv),
+    ])
+    test_transforms = tv.transforms.Compose([
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize(mean=mean, std=stdv),
+    ])
+
+    # Split training into train and validation - needed for calibration
+    #
+    # IMPORTANT! We need to use the same validation set for temperature
+    # scaling, so we're going to save the indices for later
+    train_set = tv.datasets.CIFAR100(data, train=True, transform=train_transforms, download=True)
+    valid_set = tv.datasets.CIFAR100(data, train=True, transform=test_transforms, download=False)
+    indices = torch.randperm(len(train_set))
+    train_indices = indices[:len(indices) - valid_size]
+    valid_indices = indices[len(indices) - valid_size:] if valid_size else None
+
+    # Make dataloaders
+    train_loader = torch.utils.data.DataLoader(train_set, pin_memory=True, batch_size=batch_size,
+                                               sampler=SubsetRandomSampler(train_indices))
+    valid_loader = torch.utils.data.DataLoader(valid_set, pin_memory=True, batch_size=batch_size,
+                                               sampler=SubsetRandomSampler(valid_indices))
+
+    # Make model, criterion, and optimizer
+    model = DenseNet(
+        growth_rate=growth_rate,
+        block_config=block_config,
+        num_classes=100
+    )
+    # Wrap model if multiple gpus
+    if torch.cuda.device_count() > 1:
+        model_wrapper = torch.nn.DataParallel(model).cuda()
+    else:
+        model_wrapper = model.cuda()
+    print(model_wrapper)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model_wrapper.parameters(), lr=lr, momentum=momentum, nesterov=True)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0.5 * n_epochs, 0.75 * n_epochs], gamma=0.1)
+
+    # Train model
+    best_error = 1
+    for epoch in range(1, n_epochs + 1):
+        scheduler.step()
+        run_epoch(
+            loader=train_loader,
+            model=model_wrapper,
+            criterion=criterion,
+            optimizer=optimizer,
+            epoch=epoch,
+            n_epochs=n_epochs,
+            train=True,
+        )
+        valid_results = run_epoch(
+            loader=valid_loader,
+            model=model_wrapper,
+            criterion=criterion,
+            optimizer=optimizer,
+            epoch=epoch,
+            n_epochs=n_epochs,
+            train=False,
+        )
+
+        # Determine if model is the best
+        _, _, valid_error = valid_results
+        if valid_error[0] < best_error:
+            best_error = valid_error[0]
+            print('New best error: %.4f' % best_error)
+
+            # When we save the model, we're also going to
+            # include the validation indices
+            torch.save(model.state_dict(), os.path.join(save, 'model.pth'))
+            torch.save(valid_indices, os.path.join(save, 'valid_indices.pth'))
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    """
+    Train a 40-layer DenseNet-BC on CIFAR-100
+    Args:
+        --data (str) - path to directory where data should be loaded from/downloaded
+            (default $DATA_DIR)
+        --save (str) - path to save the model to (default /tmp)
+        --valid_size (int) - size of validation set
+        --seed (int) - manually set the random seed (default None)
+    """
+    train()
