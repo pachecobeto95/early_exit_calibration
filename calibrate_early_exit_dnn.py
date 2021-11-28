a8bcee27a7c8cfe5974bc4928097ae892537d444
Roberto Pacheco <robertopvc@gmail.com>
2021-11-28 16:30:59 -0300

    s

--- calibrate_early_exit_dnn.py
+++ calibrate_early_exit_dnn.py
@@ -1563,7 +1563,7 @@ p_tar_calib = 0.8
 distribution = "linear"
 exit_type = "bnpool"
 dataset_name = "caltech256"
-model_name = "vgg16"
+model_name = "resnet18"
 root_save_path = os.path.dirname(__file__)
 
 save_indices_path = os.path.join(root_save_path, "datasets", dataset_name, "indices")
@@ -1579,7 +1579,7 @@ train_loader, val_loader, test_loader = dataset.caltech_256(dataset_path, split_
 
 early_exit_dnn = Early_Exit_DNN(model_name, n_classes, pretrained, n_branches, input_shape, exit_type, device, distribution=distribution)
 early_exit_dnn = early_exit_dnn.to(device)
-early_exit_dnn.load_state_dict(torch.load(model_save_path, map_location=device)["model_state_dict"])
+early_exit_dnn = early_exit_dnn.load_state_dict(torch.load(model_save_path, map_location=device)["model_state_dict"])
 
 result_path = os.path.join(root_save_path, "appEdge", "api", "services", "models",
 	dataset_name, model_name, "results")
