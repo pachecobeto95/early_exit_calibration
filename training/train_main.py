101e1ec11000da52707ce370965f1ccf7f92f3a0
Roberto Pacheco <robertopvc@gmail.com>
2021-11-29 11:54:50 -0300

    a

--- training/train_main.py
+++ training/train_main.py
@@ -30,6 +30,79 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
+
+class ModelWithTemperature(nn.Module):
+    """
+    A thin decorator, which wraps a model with temperature scaling
+    model (nn.Module):
+        A classification neural network
+        NB: Output of the neural network should be the classification logits,
+            NOT the softmax (or log softmax)!
+    """
+    def __init__(self, model):
+        super(ModelWithTemperature, self).__init__()
+        self.model = model
+        self.temperature = nn.Parameter(torch.ones(1) * 1.5)
+
+    def forward(self, input):
+        logits = self.model(input)
+        return self.temperature_scale(logits)
+
+    def temperature_scale(self, logits):
+        """
+        Perform temperature scaling on logits
+        """
+        # Expand temperature to match the size of logits
+        temperature = self.temperature.unsqueeze(1).expand(logits.size(0), logits.size(1))
+        return logits / temperature
+
+    # This function probably should live outside of this class, but whatever
+    def set_temperature(self, valid_loader):
+        """
+        Tune the tempearature of the model (using the validation set).
+        We're going to set it to optimize NLL.
+        valid_loader (DataLoader): validation set loader
+        """
+        self.cuda()
+        nll_criterion = nn.CrossEntropyLoss().cuda()
+        ece_criterion = _ECELoss().cuda()
+
+        # First: collect all the logits and labels for the validation set
+        logits_list = []
+        labels_list = []
+        with torch.no_grad():
+            for input, label in valid_loader:
+                input = input.cuda()
+                logits = self.model(input)
+                logits_list.append(logits)
+                labels_list.append(label)
+            logits = torch.cat(logits_list).cuda()
+            labels = torch.cat(labels_list).cuda()
+
+        # Calculate NLL and ECE before temperature scaling
+        before_temperature_nll = nll_criterion(logits, labels).item()
+        before_temperature_ece = ece_criterion(logits, labels).item()
+        print('Before temperature - NLL: %.3f, ECE: %.3f' % (before_temperature_nll, before_temperature_ece))
+
+        # Next: optimize the temperature w.r.t. NLL
+        optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=50)
+
+        def eval():
+            optimizer.zero_grad()
+            loss = nll_criterion(self.temperature_scale(logits), labels)
+            loss.backward()
+            return loss
+        optimizer.step(eval)
+
+        # Calculate NLL and ECE after temperature scaling
+        after_temperature_nll = nll_criterion(self.temperature_scale(logits), labels).item()
+        after_temperature_ece = ece_criterion(self.temperature_scale(logits), labels).item()
+        print('Optimal temperature: %.3f' % self.temperature.item())
+        print('After temperature - NLL: %.3f, ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
+
+        return self
+
+
 class LoadDataset():
   def __init__(self, input_dim, batch_size_train, batch_size_test, model_id, seed=42):
     self.input_dim = input_dim
@@ -269,7 +342,7 @@ def runMain(dataLoader, model, criterion, optimizer, epoch, n_epochs, train):
 
 
 input_resize, input_dim = 256, 224
-batch_size_train = 32
+batch_size_train = 64
 batch_size_test = 1
 model_id = 1
 split_ratio = 0.2
@@ -316,7 +389,7 @@ count = 0
 df = pd.DataFrame()
 epoch = 0
 
-while (epoch < n_epochs):
+while (epoch < n_epochs and count <= patience):
   epoch+=1
   print("Epoch: %s"%(epoch))
   result = {}
@@ -346,3 +419,5 @@ while (epoch < n_epochs):
     count += 1
 
 print("Stop! Patience is finished")
+scaled_model = ModelWithTemperature(model)
+scaled_model.set_temperature(val_loader)
