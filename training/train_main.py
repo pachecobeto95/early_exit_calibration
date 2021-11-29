ebb45bf4a5b0b09a992436374fbd0ca2b37ab4ed
Roberto Pacheco <robertopvc@gmail.com>
2021-11-29 14:26:15 -0300

    a

--- training/train_main.py
+++ training/train_main.py
@@ -103,6 +103,46 @@ class ModelWithTemperature(nn.Module):
         return self
 
 
+class _ECELoss(nn.Module):
+    """
+    Calculates the Expected Calibration Error of a model.
+    (This isn't necessary for temperature scaling, just a cool metric).
+    The input to this loss is the logits of a model, NOT the softmax scores.
+    This divides the confidence outputs into equally-sized interval bins.
+    In each bin, we compute the confidence gap:
+    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
+    We then return a weighted average of the gaps, based on the number
+    of samples in each bin
+    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
+    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
+    2015.
+    """
+    def __init__(self, n_bins=15):
+        """
+        n_bins (int): number of confidence interval bins
+        """
+        super(_ECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece
+
 class LoadDataset():
   def __init__(self, input_dim, batch_size_train, batch_size_test, model_id, seed=42):
     self.input_dim = input_dim
@@ -335,6 +375,8 @@ def runMain(dataLoader, model, criterion, optimizer, epoch, n_epochs, train):
   avg_loss = np.mean(loss_list)
 
   print("%s: %s"%('Train' if train else 'Eval', np.mean(acc_list)))
+  print("%s: %s"%('Train' if train else 'Eval', np.mean(acc_list)))
+
   mode = "train" if(train) else "val"
   return {"%s_loss"%(mode): avg_loss, "%s_acc"%(mode): avg_acc}
 
@@ -376,6 +418,7 @@ train_loader, val_loader, test_loader = dataset.caltech_256(dataset_path, split_
 
 
 model = models.resnet152(pretrained=pretrained).to(device)
+model.load_state_dict(torch.load(model_save_path, map_location=device)["model_state_dict"])
 
 criterion = nn.CrossEntropyLoss()
 
@@ -389,7 +432,7 @@ patience = 10
 count = 0
 df = pd.DataFrame()
 epoch = 0
-
+"""
 while (epoch < n_epochs and count <= patience):
   epoch+=1
   print("Epoch: %s"%(epoch))
@@ -420,5 +463,6 @@ while (epoch < n_epochs and count <= patience):
     count += 1
 
 print("Stop! Patience is finished")
+"""
 scaled_model = ModelWithTemperature(model)
 scaled_model.set_temperature(val_loader)
