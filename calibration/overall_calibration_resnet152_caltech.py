9058465fb4f5cf719dff442d6d0f29f460f363b4
Roberto Pacheco <robertopvc@gmail.com>
2021-12-01 20:54:59 -0300

    a

--- calibration/overall_calibration_resnet152_caltech.py
+++ calibration/overall_calibration_resnet152_caltech.py
@@ -28,7 +28,7 @@ import functools
 from tqdm import tqdm
 from load_dataset import LoadDataset
 from early_exit_dnn import Early_Exit_DNN
-from calibation_early_exit_dnn import ModelOverallCalibration
+from calibration_early_exit_dnn import ModelOverallCalibration
 
 
 torch.multiprocessing.set_sharing_strategy('file_system')
