7593e86a3829acd042fdba1549c0010a72c1a1cb
Roberto Pacheco <robertopvc@gmail.com>
2021-12-01 14:18:30 -0300

    a

--- training/calibrate_early_exit_resnet152_caltech.py
+++ training/calibrate_early_exit_resnet152_caltech.py
@@ -962,7 +962,117 @@ class Early_Exit_DNN(nn.Module):
 
     return actual_conf, actual_inferred_class, actual_exit_branch, conf_list, class_list, exit_branches
 
-  
+
+  def forwardAllExits(self, x):  
+
+    output_list, conf_list, infered_class_list = [], [], []
+
+    for i, exitBlock in enumerate(self.exits):
+      x = self.stages[i](x)
+
+      output_branch = exitBlock(x)
+      conf, infered_class = torch.max(self.softmax(output_branch), 1)
+
+      output_list.append(output_branch), infered_class_list.append(infered_class)
+      conf_list.append(conf)
+
+    x = self.stages[-1](x)
+    
+    x = torch.flatten(x, 1)
+
+    output = self.classifier(x)    
+    output_list.append(output)
+
+    conf, infered_class = torch.max(self.softmax(output), 1)
+
+    infered_class_list.append(infered_class), conf_list.append(conf)
+   
+    return output_list, conf_list, infered_class_list
+
+
+  def forwardOverallCalibration(self, x, temp_overall):
+    output_list, conf_list, class_list = [], [], []
+    n_exits = self.n_branches + 1
+
+    for i, exitBlock in enumerate(self.exits):
+      x = self.stages[i](x)
+      output_branch = exitBlock(x)
+      
+      output_branch = self.temperature_scale_overall(output_branch, temp_overall)
+
+      conf_branch, infered_class_branch = torch.max(self.softmax(output_branch), 1)
+
+      output_list.append(output_branch)
+      conf_list.append(conf_branch), class_list.append(infered_class_branch)
+      
+    x = self.stages[-1](x)
+    
+    x = torch.flatten(x, 1)
+
+    output = self.classifier(x)
+    output = self.temperature_scale_overall(output, temp_overall)
+    output_list.append(output)
+
+    conf, infered_class = torch.max(self.softmax(output), 1)
+    conf_list.append(conf), class_list.append(infered_class)
+
+    return output_list, conf_list, class_list
+
+  def forwardBranchesCalibration(self, x, temp_branches):
+    output_list, conf_list, class_list = [], [], []
+    n_exits = self.n_branches + 1
+
+    for i, exitBlock in enumerate(self.exits):
+      x = self.stages[i](x)
+      output_branch = exitBlock(x)
+      output_branch = self.temperature_scale_branches(output_branch, temp_branches, i)
+
+      conf_branch, infered_class_branch = torch.max(self.softmax(output_branch), 1)
+
+      output_list.append(output_branch)
+      conf_list.append(conf_branch), class_list.append(infered_class_branch)
+      
+    x = self.stages[-1](x)
+    
+    x = torch.flatten(x, 1)
+
+    output = self.classifier(x)
+    output = self.temperature_scale_branches(output, temp_branches, -1)
+    output_list.append(output)
+
+    conf, infered_class = torch.max(self.softmax(output), 1)
+    conf_list.append(conf), class_list.append(infered_class)
+
+    return output_list, conf_list, class_list
+
+  def forwardAllSamplesCalibration(self, x, temp_all_samples):
+    output_list, conf_list, class_list = [], [], []
+    n_exits = self.n_branches + 1
+
+    for i, exitBlock in enumerate(self.exits):
+      x = self.stages[i](x)
+      output_branch = exitBlock(x)
+      output_branch = self.temperature_scale_branches(output_branch, temp_all_samples, i)
+
+      conf_branch, infered_class_branch = torch.max(self.softmax(output_branch), 1)
+
+      output_list.append(output_branch)
+      conf_list.append(conf_branch), class_list.append(infered_class_branch)
+      
+    x = self.stages[-1](x)
+    
+    x = torch.flatten(x, 1)
+
+    output = self.classifier(x)
+    output = self.temperature_scale_branches(output, temp_all_samples, -1)
+    output_list.append(output)
+
+    conf, infered_class = torch.max(self.softmax(output), 1)
+    conf_list.append(conf), class_list.append(infered_class)
+
+    return output_list, conf_list, class_list
+
+
   def forwardEval(self, x, p_tar):
     """
     This method is used to train the early-exit DNN model
