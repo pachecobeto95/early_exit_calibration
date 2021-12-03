7b9ad1c29be8c6121579051ae2b0a739c6c7f61e
Roberto Pacheco <robertopvc@gmail.com>
2021-12-02 22:21:32 -0300

    a

--- calibration/calibration_early_exit_dnn.py
+++ calibration/calibration_early_exit_dnn.py
@@ -52,7 +52,7 @@ class _ECELoss(nn.Module):
             in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
             prop_in_bin = in_bin.float().mean()
             if prop_in_bin.item() > 0:
-                print(len(in_bin))
+                print(bin_lower, bin_upper, accuracy_in_bin, avg_confidence_in_bin)
                 accuracy_in_bin = accuracies[in_bin].float().mean()
                 avg_confidence_in_bin = confidences[in_bin].mean()
                 ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
