7af46504d22357e1c0cb60a69275f32fc94e4e68
Roberto Pacheco <robertopvc@gmail.com>
2021-12-05 11:53:29 -0300

    a

--- training/cifar10/utils.py
+++ training/cifar10/utils.py
@@ -35,67 +35,73 @@ class BaseBlock(nn.Module):
 		self.conv3 = nn.Conv2d(c, output_channel, kernel_size = 1, bias = False)
 		self.bn3 = nn.BatchNorm2d(output_channel)
 
-def forward(self, inputs):
-		# main path
-		x = F.relu6(self.bn1(self.conv1(inputs)), inplace = True)
-		x = F.relu6(self.bn2(self.conv2(x)), inplace = True)
-		x = self.bn3(self.conv3(x))
-		# shortcut path
-		x = x + inputs if self.shortcut else x
-		return x
+	def forward(self, inputs):
+			# main path
+			x = F.relu6(self.bn1(self.conv1(inputs)), inplace = True)
+			x = F.relu6(self.bn2(self.conv2(x)), inplace = True)
+			x = self.bn3(self.conv3(x))
+			# shortcut path
+			x = x + inputs if self.shortcut else x
+			return x
 
 
 class MobileNetV2(nn.Module):
-    def __init__(self, output_size, alpha = 1):
-        super(MobileNetV2, self).__init__()
-        self.output_size = output_size
+	def __init__(self, n_classes, alpha = 1):
+		super(MobileNetV2, self).__init__()
+		self.n_classes = n_classes
 
-        # first conv layer 
-        self.conv0 = nn.Conv2d(3, int(32*alpha), kernel_size = 3, stride = 1, padding = 1, bias = False)
-        self.bn0 = nn.BatchNorm2d(int(32*alpha))
+		# first conv layer 
+		#self.conv0 = nn.Conv2d(3, int(32*alpha), kernel_size = 3, stride = 1, padding = 1, bias = False)
+		#self.bn0 = nn.BatchNorm2d(int(32*alpha))
 
-        # build bottlenecks
-        BaseBlock.alpha = alpha
-        self.bottlenecks = nn.Sequential(
-            BaseBlock(32, 16, t = 1, downsample = False),)
+		# build bottlenecks
+		BaseBlock.alpha = alpha
+		self.bottlenecks = nn.Sequential(
+			nn.Conv2d(3, int(32*alpha), kernel_size = 3, stride = 1, padding = 1, bias = False),
+			nn.BatchNorm2d(int(32*alpha)),
+			nn.ReLU6(inplace = True),
+			BaseBlock(32, 16, t = 1, downsample = False),
+			BaseBlock(16, 24, downsample = False),
+			BaseBlock(24, 24),
+			BaseBlock(24, 32, downsample = False),
+			BaseBlock(32, 32),
+			BaseBlock(32, 32),
+			BaseBlock(32, 64, downsample = True),
+			BaseBlock(64, 64),
+			BaseBlock(64, 64),
+			BaseBlock(64, 64),
+			BaseBlock(64, 96, downsample = False),
+			BaseBlock(96, 96),
+			BaseBlock(96, 96),
+			BaseBlock(96, 160, downsample = True),
+			BaseBlock(160, 160),
+			BaseBlock(160, 160),
+			BaseBlock(160, 320, downsample = False),
+			nn.Conv2d(int(320*alpha), 1280, kernel_size = 1, bias = False),
+			nn.BatchNorm2d(1280),
+			)
 
-        # last conv layers and fc layer
-        self.conv1 = nn.Conv2d(int(320*alpha), 1280, kernel_size = 1, bias = False)
-        self.bn1 = nn.BatchNorm2d(1280)
-        self.fc = nn.Linear(1280, output_size)
+		self.classifier = nn.Linear(1280, n_classes)
 
-        # weights init
-        self.weights_init()
+		# weights init
+		self.weights_init()
 
 
-    def weights_init(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+	def weights_init(self):
+		for m in self.modules():
+			if isinstance(m, nn.Conv2d):
+				n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+				m.weight.data.normal_(0, math.sqrt(2. / n))
 
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                #m.bias.data.zero_()
+			elif isinstance(m, nn.BatchNorm2d):
+				m.weight.data.fill_(1)
+				#m.bias.data.zero_()
 
 
-    def forward(self, inputs):
+	def forward(self, inputs):
 
-        # first conv layer
-        x = F.relu6(self.bn0(self.conv0(inputs)), inplace = True)
-        # assert x.shape[1:] == torch.Size([32, 32, 32])
+		x = self.bottlenecks(x)
+		x = x.view(x.shape[0], -1)
+		x = self.classifier(x)
 
-        # bottlenecks
-        x = self.bottlenecks(x)
-        # assert x.shape[1:] == torch.Size([320, 8, 8])
-
-        # last conv layer
-        x = F.relu6(self.bn1(self.conv1(x)), inplace = True)
-        # assert x.shape[1:] == torch.Size([1280,8,8])
-
-        # global pooling and fc (in place of conv 1x1 in paper)
-        x = F.adaptive_avg_pool2d(x, 1)
-        x = x.view(x.shape[0], -1)
-        x = self.fc(x)
-
-        return x
+		return x
