ba142eb11721fd394749e46c1813a7aec07cfb6a
Roberto Pacheco <robertopvc@gmail.com>
2021-12-10 17:07:56 -0300

    a

--- training/cifar10/train_early_exit_mobilenet.py
+++ training/cifar10/train_early_exit_mobilenet.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 	loss_weights = loss_dict[args.loss_weight_type]
 
 	early_exit_dnn = Early_Exit_DNN(model_name, n_classes, args.pretrained, args.backbone_pretrained, 
-		backbone_model_path, args.n_branches, input_shape, exit_type, device, distribution=distribution)
+		backbone_model_path, args.n_branches, input_shape, args.exit_type, device, distribution=args.distribution)
 	early_exit_dnn = early_exit_dnn.to(device)
 
 	if(args.dataset_name=="cifar10"):
