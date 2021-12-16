bf4aaf2379acc8997fb7a53fd83c9b122c5c4474
Roberto Pacheco <robertopvc@gmail.com>
2021-12-16 17:39:58 -0300

    a

--- training/cifar10/calibration_dnn.py
+++ training/cifar10/calibration_dnn.py
@@ -452,11 +452,11 @@ class ModelAllSamplesCalibration(nn.Module):
 
 def calibratingEEModels(model, val_loader, p_tar, device, model_path, temperaturePath, args):
 
-    #overall_model = ModelOverallCalibration(model, device, model_path, temperaturePath["overall_calib"], args.lr, args.max_iter)
-    #overall_model.set_temperature(val_loader, p_tar)
+    overall_model = ModelOverallCalibration(model, device, model_path, temperaturePath["overall_calib"], args.lr, args.max_iter)
+    overall_model.set_temperature(val_loader, p_tar)
 
-    #branches_model = ModelBranchesCalibration(model, device, model_path, temperaturePath["branches_calib"], args.lr, args.max_iter)
-    #branches_model.set_temperature(val_loader, p_tar)
+    branches_model = ModelBranchesCalibration(model, device, model_path, temperaturePath["branches_calib"], args.lr, args.max_iter)
+    branches_model.set_temperature(val_loader, p_tar)
 
     all_samples_model = ModelAllSamplesCalibration(model, device, model_path, temperaturePath["all_samples_calib"], args.lr, args.max_iter)
     all_samples_model.set_temperature(val_loader, p_tar)
