5aee84bec3e16815b2eb7b6af5d3bbd1b27ba362
Roberto Pacheco <robertopvc@gmail.com>
2021-12-21 20:52:45 -0300

    a

--- training/cifar10/calibrate_early_exit_dnn.py
+++ training/cifar10/calibrate_early_exit_dnn.py
@@ -71,7 +71,7 @@ if (__name__ == "__main__"):
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 	input_shape = (3, input_size, input_size)
 
-	threshold_list = [0.5, 0.6, 0.7, 0.8, 0.9]
+	threshold_list = [0.8, 0.9]
 
 	backbone_model_path = os.path.join(model_dir_path, "%s_main_%s_id_%s_%s.pth"%(args.model_name, args.dataset_name, args.model_id, mode))
 	model_path = os.path.join(model_dir_path, "b_%s_early_exit_%s_id_%s_%s_%s.pth"%(args.model_name, args.dataset_name, args.model_id, mode, args.loss_weight_type))
