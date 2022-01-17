9e5a2791e0d6723a19913fec8e91055bbcfbc7a6
Roberto Pacheco <robertopvc@gmail.com>
2022-01-17 13:42:44 -0300

    a

--- training/cifar10/load_dataset.py
+++ training/cifar10/load_dataset.py
@@ -99,4 +99,4 @@ def loadCifar100(root_path, indices_path, model_id, batch_size_train, batch_size
 	testset = CIFAR100(root_path, transform=transform_test, train=False, download=True)
 	testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size_test, shuffle = False, num_workers=4)
 
-	return train_loader, val_loader, train_loader
+	return train_loader, val_loader, testloader
