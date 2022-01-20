6064332ab1e29924a4bb269049af57ede08a8a78
Roberto Pacheco <robertopvc@gmail.com>
2022-01-20 16:18:02 -0300

    a

--- extra/ucb_exp_label_conf_bin.py
+++ extra/ucb_exp_label_conf_bin.py
@@ -117,8 +117,8 @@ if __name__ == "__main__":
 	threshold_list = np.arange(0, 1.1, 0.1)
 	overhead_list = np.arange(0, 1.1, 0.1)
 	verbose = False
-	savePath = os.path.join(".", "ucb_bin_conf_result_c_%s_2022.csv"%(args.c))
-	logPath = os.path.join(".", "logUCBConfBin_2022_2.csv")
+	savePath = os.path.join(".", "ucb_bin_conf_result_c_%s_2022_2.csv"%(args.c))
+	logPath = os.path.join(".", "logUCBConfBin_2022.txt")
 	ucb_experiment(df_result, threshold_list, overhead_list, args.n_rounds, args.c, savePath, logPath, verbose)
 
 
