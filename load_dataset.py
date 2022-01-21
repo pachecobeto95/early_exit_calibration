b9d9dd0cf0b9d136dbd651a499d335e6d65ce156
Roberto Pacheco <robertopvc@gmail.com>
2022-01-21 12:45:02 -0300

    a

---
+++ load_dataset.py
@@ -0,0 +1,42 @@
+
+
+
+def load_test_caltech_256(input_dim, dataset_path, split_ratio, savePath_idx):
+	# This method loads the Caltech-256 dataset.
+
+
+	#To normalize the input images data.
+	mean = [0.457342265910642, 0.4387686270106377, 0.4073427106250871]
+	std =  [0.26753769276329037, 0.2638145880487105, 0.2776826934044154]
+
+	# Note that we do not apply data augmentation in the test dataset.
+	self.transformations_test = transforms.Compose([
+		transforms.Resize(input_dim), 
+		transforms.ToTensor(), 
+		#transforms.Normalize(mean = mean, std = std),
+		])
+
+
+	torch.manual_seed(self.seed)
+	np.random.seed(seed=self.seed)
+
+	train_set = datasets.ImageFolder(dataset_path)
+
+	val_set = datasets.ImageFolder(dataset_path, transform=self.transformations_test)
+    
+	test_set = datasets.ImageFolder(dataset_path, transform=self.transformations_test)
+
+	if (os.path.exists(os.path.join(savePath_idx, "test_idx_%s_id_%s.npy"%(dataset_name, self.model_id)))):
+		test_idx = np.load(os.path.join(savePath_idx, "test_idx_%s_id_%s.npy"%(dataset_name, self.model_id)), allow_pickle=True)
+		test_idx = np.array(list(test_idx.tolist()))
+	else:
+		_, test_idx = self.get_indices(train_set, split_ratio)
+
+
+	test_data = torch.utils.data.Subset(test_set, indices=test_idx)
+
+	test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, num_workers=4)
+
+	return test_loader 
+
+
