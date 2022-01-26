db6dac88623a65fbba0f09c4ecd4668c5379015e
Roberto Pacheco <robertopvc@gmail.com>
2022-01-26 11:22:04 -0300

    a

--- extra/ucb_exp_single_image.py
+++ extra/ucb_exp_single_image.py
@@ -39,7 +39,7 @@ def run_ucb(df, threshold_list, overhead, n_rounds, c, bin_lower, bin_upper, sav
 	df_size = len(df)
 	indices_list = np.arange(df_size)
 	idx = 0	
-	
+
 	for n_round in range(n_rounds):
 
 		row = df.iloc[[idx]]
@@ -72,7 +72,8 @@ def run_ucb(df, threshold_list, overhead, n_rounds, c, bin_lower, bin_upper, sav
 		selected_arm_list[n_round] = round(threshold, 2) 
 
 		if (n_round%100000 == 0):
-			print("N Round: %s, Overhead: %s"%(n_round, overhead), file=open(logPath, "a"))
+			print("N Round: %s, Overhead: %s, Bin:[%s, %s]"%(n_round, overhead, bin_lower, bin_upper), 
+				file=open(logPath, "a"))
 
 
 	result = {"selected_arm": selected_arm_list, "regret": inst_regret_list, 
@@ -92,11 +93,14 @@ def ucb_experiment(df, threshold_list, overhead_list, n_round, c, savePath, logP
 
 	for overhead in overhead_list:
 		for bin_lower, bin_upper in zip(bin_lowers, bin_uppers):
+			print("Overhead: %s, Bin:[%s, %s]"%(overhead, bin_lower, bin_upper), file=open(logPath, "a"))
 
 			df_temp = df[(df.conf_branch_1 >= bin_lower) & (df.conf_branch_1 <= bin_upper)] 
 			
 			if(len(df_temp.conf_branch_1.values) > 0):
 				result = run_ucb(df_temp, threshold_list, overhead, n_round, c, bin_lower, bin_upper, savePath, logPath, verbose)
+				
+				#Save Results
 				df2 = pd.DataFrame(np.array(list(result.values())).T, columns=list(result.keys()))
 				df_result = df_result.append(df2)
 				df_result.to_csv(savePath)
@@ -120,6 +124,3 @@ if __name__ == "__main__":
 	savePath = os.path.join(".", "ucb_single_image_bin_conf_result_c_%s.csv"%(args.c))
 	logPath = os.path.join(".", "logUCBSingleImage.txt")
 	ucb_experiment(df_result, threshold_list, overhead_list, args.n_rounds, args.c, savePath, logPath, verbose)
-
-
-
