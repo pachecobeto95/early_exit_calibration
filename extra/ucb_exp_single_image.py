b3dea2c0e7d5cef42e2a832787f64780d5c37c0f
Roberto Pacheco <robertopvc@gmail.com>
2022-02-01 21:43:17 -0300

    a

--- extra/ucb_exp_single_image.py
+++ extra/ucb_exp_single_image.py
@@ -123,6 +123,6 @@ if __name__ == "__main__":
 	threshold_list = np.arange(0, 1.1, 0.1)
 	overhead_list = np.arange(0, 1.1, 0.1)
 	verbose = False
-	savePath = os.path.join(".", "ucb_single_image_bin_conf_result_c_%s.csv"%(args.c))
+	savePath = os.path.join(".", "ucb_single_image_bin_conf_result_c_%s_2.csv"%(args.c))
 	logPath = os.path.join(".", "logUCBSingleImage.txt")
 	ucb_experiment(df_result, threshold_list, overhead_list, args.n_rounds, args.c, savePath, logPath, verbose)
