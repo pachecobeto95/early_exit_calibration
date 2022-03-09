0cd829fbcc44fb1ea9fba1fb67b679d1bf62d561
Roberto Pacheco <robertopvc@gmail.com>
2022-03-09 14:58:24 -0300

    a

--- collect_confidence.py
+++ collect_confidence.py
@@ -104,7 +104,7 @@ def main(args):
 	p_tar_list = [0.7, 0.8, 0.9]
 	dataset_path = os.path.join(DIR_NAME, "datasets", "caltech256", "256_ObjectCategories")
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-	savePath = os.path.join(DIR_NAME, "confidence_branches_%s_%s.csv"%(args.model_name, args.dataset_name))
+	savePath = os.path.join(DIR_NAME, "osvaldo_experiments", "confidence_branches_%s_%s.csv"%(args.model_name, args.dataset_name))
 	edge_root_path = os.path.join(DIR_NAME, "appEdge", "api", "services", "models")
 
 	#This line defines the number of side branches processed at the edge
@@ -121,7 +121,6 @@ def main(args):
 	save_indices_path = os.path.join(DIR_NAME, "datasets", args.dataset_name, "indices", "test_idx_caltech256_id_1.npy")
 
 	test_loader = load_dataset(args, dataset_path, save_indices_path)
-	sys.exit()
 	collectingConfidenceExperiment(test_loader, early_exit_dnn, p_tar_list, nr_branch_edge, device, savePath)
 
 
