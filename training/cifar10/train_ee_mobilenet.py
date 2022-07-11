62bb2e9dbe25c23fa2152a24bea63f4ed33a989f
Roberto Pacheco <robertopvc@gmail.com>
2022-07-11 11:26:45 -0300

    a

--- training/cifar10/train_ee_mobilenet.py
+++ training/cifar10/train_ee_mobilenet.py
@@ -202,7 +202,7 @@ class Early_Exit_DNN(nn.Module):
 		intermediate_model = nn.Sequential(*(list(self.stages)+list(self.layers)))
 		x = torch.rand(1, 3, self.width, self.height).to(self.device)
 		#current_flop, _ = count_ops(intermediate_model, x, verbose=False, print_readable=False)
-		current_flop, _ = get_model_complexity_info(intermediate_model, x, as_strings=True,
+		current_flop, _ = get_model_complexity_info(intermediate_model, x[0], as_strings=True,
 			print_per_layer_stat=True, verbose=True)
 		return self.stage_id < self.n_branches and current_flop >= self.threshold_flop_list[self.stage_id]
 
